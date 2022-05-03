9b79c2079f32af3201307aa44c505e3f2d86d4ab
andrzmc <drzxax@gmail.com>
2022-05-02 19:29:44 -0500

    feat: add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarModule } from './shared/components/layouts/navbar/navbar.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NavbarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
