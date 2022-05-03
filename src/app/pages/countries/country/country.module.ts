7e8a528801bec9dfe27ebdf0e27f44d74c968181
andrzmc <drzxax@gmail.com>
2022-05-02 21:50:39 -0500

    feat: add new components for country

---
+++ src/app/pages/countries/country/country.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CountryComponent } from './country.component';
+import { CountryRoutingModule } from './country-routing.module';
+
+@NgModule({
+  declarations: [CountryComponent],
+  imports: [CommonModule, CountryRoutingModule],
+})
+export class CountryModule {}
